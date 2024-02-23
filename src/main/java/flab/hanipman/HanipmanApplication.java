c06b907b049b38ec1ccbaeb9e6b87a01d71c5608
Jasonarea <hyeonsuns@naver.com>
2024-02-24 08:54:14 +0900

    initial project setup

---
+++ src/main/java/flab/hanipman/HanipmanApplication.java
@@ -0,0 +1,13 @@
+package flab.hanipman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanipmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HanipmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/flab/hanipman/HanipmanApplicationTests.java
@@ -0,0 +1,13 @@
+package flab.hanipman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanipmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
